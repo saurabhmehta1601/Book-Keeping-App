7ecc7c38e9f366e483e089a6c2a5ed570e4b3d2a
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-03-06 17:20:37 +0530

    user model and schema created

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose =require('mongoose')
+
+
+// Schema
+
+const UserSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+
+const User =new mongoose.model('User',UserSchema)
+
+module.exports = User
