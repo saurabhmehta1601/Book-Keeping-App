a1cccc161d550a7eefcd2dc778a4f16afd8e32e3
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-03-07 11:01:33 +0530

    book model and schema created

---
+++ backend/models/Book.js
@@ -0,0 +1,28 @@
+const mongoose =require('mongoose')
+
+const bookSchema= new mongoose.Schema({
+    category:{
+        type:String,
+        required:[true,'Book category is required']
+    },
+    author:{
+        type:String,
+        required:[true,'Book author is required']
+    },
+    title:{
+        type:String,
+        required:[true,'Book title is required']
+    },
+    addedBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required :true
+    }
+},{
+    timestamps:true
+})
+
+const Book =new mongoose.model('Book',bookSchema)
+
+
+module.exports=Book
