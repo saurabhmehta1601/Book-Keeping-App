7b12e97432a7aaf04304fe2136b6fc0bc29a0d90
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-03-07 10:34:03 +0530

    generated token using jwt

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,56 +1,65 @@
-const express=require('express')
-const User =require('../models/User')
-const asyncHandler = require('express-async-handler')
-const usersRoute=express.Router()
-
+const express = require("express");
+const User = require("../models/User");
+const asyncHandler = require("express-async-handler");
+const usersRoute = express.Router();
+const generateToken = require("../utils/generateToken");
 
 // Register
-usersRoute.post("/register",asyncHandler( async (req,res)=>{
-    
-        const {name,email,password} = req.body
-        const userExists =await User.findOne({email:email})
+usersRoute.post(
+  "/register",
+  asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email: email });
 
-        if(userExists){
-            throw new Error('User with given email already exists')
-        }
-        const createdUser =await User.create({name,email,password})
-        res.send(createdUser)
-}))
+    if (userExists) {
+      throw new Error("User with given email already exists");
+    }
+    const createdUser = await User.create({ name, email, password });
+    res.send({
+      id: createdUser.id,
+      name: createdUser.name,
+      email: createdUser.email,
+      token: generateToken(createdUser.id),
+    });
+  })
+);
 
 // Login
-usersRoute.post('/login',asyncHandler( async (req,res) => {
+usersRoute.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
 
-    const {email,password} = req.body
+    const user = await User.findOne({ email: email });
 
-    const user= await User.findOne({email:email})
-
-    if(user && (await user.isPasswordMatch(password))){
-        res.status(200)
-        res.json({
-            id:user.id
-        })
-    }else{
-        res.status(401)
-        throw new Error('Invalid credentials')
+    if (user && (await user.isPasswordMatch(password))) {
+      res.status(200);
+      res.json({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user.id),
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid credentials");
     }
-})
-)
+  })
+);
 
 // update user
-usersRoute.put('/update',(req,res)=>{
-    res.send('user update request made')
-})
+usersRoute.put("/update", (req, res) => {
+  res.send("user update request made");
+});
 
 // delete user
-usersRoute.delete('/:id',(req,res)=>{
-    res.send('deleted user request made')
-})
-
+usersRoute.delete("/:id", (req, res) => {
+  res.send("deleted user request made");
+});
 
 // fetch users
-usersRoute.get('/',(req,res)=>{
-    res.send('fetched users')
-})
+usersRoute.get("/", (req, res) => {
+  res.send("fetched users");
+});
 
-
-module.exports=usersRoute
+module.exports = usersRoute;
